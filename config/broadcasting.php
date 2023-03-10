2d34a9270cd333307aef6f0b1d592749e8d5d343
izzuddin <dinruzman@gmail.com>
2023-03-10 17:28:43 +0800

    push

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,7 +36,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST','192.168.17.99'),
+                'host' => env('PUSHER_HOST','127.0.0.1'),
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 // 'encrypted' => false,
